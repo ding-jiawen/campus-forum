97857311d6630b859d481bdd98b3b4190b803acb
Ding Jiawen <2080944758@qq.com>
2025-03-21 12:03:29 +0800

    配置SpringSecurity基本内容

---
+++ my-project-backend/src/main/java/com/example/entity/RestBean.java
@@ -0,0 +1,23 @@
+package com.example.entity;
+
+import com.alibaba.fastjson2.JSONObject;
+import com.alibaba.fastjson2.JSONWriter;
+
+public record RestBean<T>(int code, T data, String message) {
+    // 成功 带数据
+    public static <T> RestBean<T> success(T data) {
+        return new RestBean<>(200, data, "请求成功");
+    }
+    // 成功 不带数据
+    public static <T> RestBean<T> success() {
+        return success(null);
+    }
+
+    public static <T> RestBean<T> failure(int code, String message) {
+        return new RestBean<>(401, null, message);
+    }
+
+    public String asJsonString() {
+        return JSONObject.toJSONString(this, JSONWriter.Feature.WriteNulls);
+    }
+}
