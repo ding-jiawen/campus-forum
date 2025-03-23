f1c0eb150ab3d25294cc8bee8eefd0f1df4c5b36
Ding Jiawen <2080944758@qq.com>
2025-03-23 16:22:40 +0800

    前端基本页面配置，登录界面ä½左侧

---
+++ my-project-frontend/src/views/WelcomeView.vue
@@ -0,0 +1,37 @@
+<script setup>
+</script>
+
+<template>
+  <div style="width: 100vw; height: 100vh; overflow: hidden; display: flex">
+    <div style="flex: 1">
+      <el-image style="width: 100%; height: 100%" fit="cover"
+          src="https://haowallpaper.com/link/common/file/previewFileImg/16477395881414016"/>
+    </div>
+    <div class="welcome-title">
+      <div style="font-size: 24px; font-weight: bold">欢迎来到我们的学习平台</div>
+      <div style="margin-top: 10px">在这里你可以学习如何使用Java，如何搭建网站，并且与Java之父密切交流</div>
+      <div style="margin-top: 5px">在这里你可以同性交友，因为都是男的，没有学Java的女生</div>
+    </div>
+    <div class="right-card">
+
+    </div>
+
+  </div>
+</template>
+
+<style scoped>
+
+.welcome-title {
+  position: absolute;
+  bottom: 30px;
+  left: 30px;
+  color: white;
+  text-shadow: 0 0 10px black;
+}
+
+.right-card {
+  width: 400px;
+  z-index: 1;
+  background-color: white;
+}
+</style>
---
+++ my-project-frontend/src/views/welcome/LoginPage.vue
@@ -0,0 +1,12 @@
+<script setup>
+</script>
+
+<template>
+  <div>
+    woshidluyemian
+  </div>
+</template>
+
+<style scoped>
+
+</style>
