90c8b2440b6e59321675e6610f028c6013af8200
Ding Jiawen <2080944758@qq.com>
2025-03-27 14:56:59 +0800

    验证码参数校验

--- my-project-backend/src/main/java/com/example/controller/AuthorizeController.java
+++ my-project-backend/src/main/java/com/example/controller/AuthorizeController.java
@@ -4,10 +4,15 @@ import com.example.entity.RestBean;
 import com.example.service.AccountService;
 import jakarta.annotation.Resource;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@Validated
 @RestController
 @RequestMapping("/api/auth")
 public class AuthorizeController {
@@ -16,8 +21,10 @@ public class AuthorizeController {
     AccountService accountService;
 
     @GetMapping("/ask-code")
-    public RestBean<Void> askVerifyCode(String email, String type, HttpServletRequest request) {
+    public RestBean<Void> askVerifyCode(@RequestParam @Email String email,
+                                        @RequestParam @Pattern(regexp = "register|reset") String type,
+                                        HttpServletRequest request) {
         String message = accountService.registerEmailVerifyCode(type, email, request.getRemoteAddr());
         return message == null ? RestBean.success() : RestBean.failure(400, message);
     }
- }
+}
